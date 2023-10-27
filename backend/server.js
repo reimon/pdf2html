ce85bb665bc4bae0ba48f9a742cddae0f6bd0a30
Reimon Gonçalves <remon@MacBook-Pro-de-Reimon.local>
2023-10-27 20:58:10 -0300

    iniciando

--- backend/server.js
+++ backend/server.js
@@ -17,34 +17,32 @@ app.post('/upload', (req, res) => {
     console.log('Receiving file...');
 
     if (!req.files || Object.keys(req.files).length === 0) {
-        console.log('No file uploaded.');
+        console.log('No file uploaded');
         return res.status(400).send('No files were uploaded.');
     }
 
     let uploadedFile = req.files.pdfFile;
-    let timestamp = new Date().getTime();
-    let uniqueFileName = `${timestamp}-${uploadedFile.name}`;
-    let uploadPath = path.join(uploadsDir, uniqueFileName);
-    let convertedFileName = uniqueFileName.replace('.pdf', '.html');
-    let convertedPath = path.join(convertedDir, convertedFileName);
+    let uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    let uploadPath = path.join(uploadsDir, uniqueName + '.pdf');
+    let convertedPath = path.join(convertedDir, uniqueName + '.html');
 
-    console.log(`Uploading file: ${uniqueFileName}`);
+    console.log(`Saving file as: ${uploadPath}`);
 
     uploadedFile.mv(uploadPath, function(err) {
         if (err) {
-            console.error('File upload failed:', err);
+            console.error('Error saving the file:', err);
             return res.status(500).send(err);
         }
 
-        console.log(`File uploaded. Starting conversion: ${uniqueFileName}`);
+        console.log(`File saved. Starting conversion to HTML: ${convertedPath}`);
 
         exec(`pdf2htmlEX --embed cfijo ${uploadPath} ${convertedPath}`, (err, stdout, stderr) => {
             if (err) {
-                console.error('Conversion failed:', err);
+                console.error('Error in conversion process:', err);
                 return res.status(500).send('Error in conversion process.');
             }
-            console.log(`Conversion successful: ${convertedFileName}`);
-            res.send({filePath: `converted/${convertedFileName}`});
+            console.log('Conversion complete.');
+            res.send({filePath: `converted/${uniqueName}.html`});
         });
     });
 });
