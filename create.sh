ca10964832305e08af2342a522bafcac21fcc22d
Reimon Gonçalves <remon@MacBook-Pro-de-Reimon.local>
2023-10-27 20:51:41 -0300

    iniciando

--- create.sh
+++ create.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Definindo o nome do diretório principal
-PROJECT_DIR="pdf2html"
-
 # Criando a estrutura de diretórios
 echo "Criando a estrutura de diretórios para o projeto $PROJECT_DIR..."
 
-mkdir -p "$PROJECT_DIR"
 
 # Diretórios do backend
-mkdir -p "$PROJECT_DIR/backend/uploads"
+mkdir -p "backend/uploads"
 
 # Diretórios do frontend
-mkdir -p "$PROJECT_DIR/frontend/public"
-mkdir -p "$PROJECT_DIR/frontend/converted"
+mkdir -p "frontend/public"
+mkdir -p "frontend/converted"
 
 # Mensagem de conclusão
 echo "Estrutura de diretórios criada com sucesso."
 
-# Listando a estrutura criada
-tree "$PROJECT_DIR"
+echo "Instalando sistema"
+npm install -y
