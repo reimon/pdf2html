6638ec4f32084f013ebf09df84a8826d181fb569
Reimon Gonçalves <remon@MacBook-Pro-de-Reimon.local>
2023-10-27 21:05:19 -0300

    iniciando

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ app.use(express.static('frontend/public'));
 const uploadsDir = path.join(__dirname, 'uploads');
 const convertedDir = path.join(__dirname, '..', 'frontend/converted');
 
+// Ensure uploads and converted directories exist
+fs.mkdirSync(uploadsDir, { recursive: true });
+fs.mkdirSync(convertedDir, { recursive: true });
+
+// Endpoint to handle file uploads
 app.post('/upload', (req, res) => {
     console.log('Receiving file...');
 
@@ -22,9 +27,8 @@ app.post('/upload', (req, res) => {
     }
 
     let uploadedFile = req.files.pdfFile;
-    let uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    let uploadPath = path.join(uploadsDir, uniqueName + '.pdf');
-    let convertedPath = path.join(convertedDir, uniqueName + '.html');
+    let uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.pdf';
+    let uploadPath = path.join(uploadsDir, uniqueName);
 
     console.log(`Saving file as: ${uploadPath}`);
 
@@ -34,16 +38,27 @@ app.post('/upload', (req, res) => {
             return res.status(500).send(err);
         }
 
-        console.log(`File saved. Starting conversion to HTML: ${convertedPath}`);
+        console.log(`File saved at ${uploadPath}`);
+        res.send({filePath: `uploads/${uniqueName}`});
+    });
+});
 
-        exec(`pdf2htmlEX  ${uploadPath} ${convertedPath}`, (err, stdout, stderr) => {
-            if (err) {
-                console.error('Error in conversion process:', err);
-                return res.status(500).send('Error in conversion process.');
-            }
-            console.log('Conversion complete.');
-            res.send({filePath: `converted/${uniqueName}.html`});
-        });
+// Endpoint for converting PDF to HTML
+app.get('/convert', (req, res) => {
+    let uploadedFilePath = req.query.file;
+    let fileName = path.basename(uploadedFilePath, '.pdf');
+    let uploadPath = path.join(uploadsDir, fileName + '.pdf');
+    let convertedPath = path.join(convertedDir, fileName + '.html');
+
+    console.log(`Starting conversion of ${uploadPath} to HTML`);
+
+    exec(`pdf2htmlEX --embed cfijo ${uploadPath} ${convertedPath}`, (err, stdout, stderr) => {
+        if (err) {
+            console.error('Error in conversion process:', err);
+            return res.status(500).send('Error in conversion process.');
+        }
+        console.log('Conversion complete.');
+        res.send({filePath: `converted/${fileName}.html`});
     });
 });
 
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,14 +1,29 @@
-document.getElementById('uploadForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+const uploadBtn = document.getElementById('uploadBtn');
+const convertBtn = document.getElementById('convertBtn');
+let uploadedFilePath = '';
 
-    var formData = new FormData(this);
+uploadBtn.addEventListener('click', function() {
+    var formData = new FormData(document.getElementById('uploadForm'));
     fetch('/upload', {
         method: 'POST',
         body: formData
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('result').innerHTML = `<a href="${data.filePath}" target="_blank">View Converted HTML</a>`;
+        uploadedFilePath = data.filePath;
+        document.getElementById('result').innerHTML = `<a href="${uploadedFilePath}" target="_blank">Download Uploaded PDF</a>`;
+        convertBtn.disabled = false;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
+
+convertBtn.addEventListener('click', function() {
+    fetch(`/convert?file=${uploadedFilePath}`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result').innerHTML += `<br><a href="${data.filePath}" target="_blank">View Converted HTML</a>`;
     })
     .catch(error => {
         console.error('Error:', error);
