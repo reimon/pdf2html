808508fc9ad597616ec027ed679a9c67bc8a1456
Reimon Gonçalves <remon@MacBook-Pro-de-Reimon.local>
2023-10-27 21:17:23 -0300

    iniciando

--- backend/server.js
+++ backend/server.js
@@ -8,61 +8,66 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 app.use(fileUpload());
-app.use(express.static('frontend/public'));
 
+// Diretórios para armazenar e servir os arquivos
 const uploadsDir = path.join(__dirname, 'uploads');
-const convertedDir = path.join(__dirname, '..', 'frontend/converted');
+const convertedDir = path.join(__dirname, 'frontend', 'converted');
 
-// Ensure uploads and converted directories exist
+// Criar os diretórios se eles não existirem
 fs.mkdirSync(uploadsDir, { recursive: true });
 fs.mkdirSync(convertedDir, { recursive: true });
 
-// Endpoint to handle file uploads
+// Configuração para servir arquivos estáticos
+app.use(express.static('frontend/public'));
+app.use('/uploads', express.static(uploadsDir));
+app.use('/converted', express.static(convertedDir));
+
+// Endpoint para upload de arquivos
 app.post('/upload', (req, res) => {
-    console.log('Receiving file...');
+    console.log('Recebendo arquivo...');
 
     if (!req.files || Object.keys(req.files).length === 0) {
-        console.log('No file uploaded');
-        return res.status(400).send('No files were uploaded.');
+        console.log('Nenhum arquivo foi enviado.');
+        return res.status(400).send('Nenhum arquivo foi enviado.');
     }
 
     let uploadedFile = req.files.pdfFile;
     let uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.pdf';
     let uploadPath = path.join(uploadsDir, uniqueName);
 
-    console.log(`Saving file as: ${uploadPath}`);
+    console.log(`Salvando arquivo como: ${uploadPath}`);
 
     uploadedFile.mv(uploadPath, function(err) {
         if (err) {
-            console.error('Error saving the file:', err);
+            console.error('Erro ao salvar o arquivo:', err);
             return res.status(500).send(err);
         }
 
-        console.log(`File saved at ${uploadPath}`);
+        console.log(`Arquivo salvo em ${uploadPath}`);
         res.send({filePath: `uploads/${uniqueName}`});
     });
 });
 
-// Endpoint for converting PDF to HTML
+// Endpoint para converter PDF em HTML
 app.get('/convert', (req, res) => {
     let uploadedFilePath = req.query.file;
     let fileName = path.basename(uploadedFilePath, '.pdf');
     let uploadPath = path.join(uploadsDir, fileName + '.pdf');
     let convertedPath = path.join(convertedDir, fileName + '.html');
 
-    console.log(`Starting conversion of ${uploadPath} to HTML`);
+    console.log(`Iniciando conversão de ${uploadPath} para HTML`);
 
-    exec(`pdf2htmlEX  ${uploadPath} ${convertedPath}`, (err, stdout, stderr) => {
+    exec(`pdf2htmlEX --embed cfijo ${uploadPath} ${convertedPath}`, (err, stdout, stderr) => {
         if (err) {
-            console.error('Error in conversion process:', err);
-            return res.status(500).send('Error in conversion process.');
+            console.error('Erro no processo de conversão:', err);
+            return res.status(500).send('Erro no processo de conversão.');
         }
-        console.log('Conversion complete.');
+        console.log('Conversão completa.');
         res.send({filePath: `converted/${fileName}.html`});
     });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
